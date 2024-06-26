6a7abfd790c0639b9140bee2ec4e92cee53e69f0
FitriSulistiawati <fitri.sulistiawati_ti22@nusaputra.ac.id>
2024-06-26 15:37:32 +0700

    first commit

---
+++ barang/Barang.java
@@ -0,0 +1,39 @@
+package barang;
+class Barang {
+    private String kode;
+    private String nama;
+    private String harga;
+    private String stok;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getStok() {
+        return stok;
+    }
+
+    public void setStok(String stok) {
+        this.stok = stok;
+    }
+}
